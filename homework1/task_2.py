45f9c01f849f5e1286f851923141b88242040037
TamarDolaberidze <dolaberidzetamari@gmail.com>
2024-09-11 21:32:12 +0400

    homework-1-branch

--- homework1/task_2.py
+++ homework1/task_2.py
@@ -1,6 +1,6 @@
 name = input("Enter a name: ")
 birth_year = input("Enter a birth year: ")
-age_now = 2024 - int(birth_year)
+age_now = 2024 - int(birth_year) 
 print ("გამარჯობა,", name, "თქვენ ხართ", age_now, "წლის.")
 
 
