2f3de1ee132896c7284e086092d95b9f71a5e43e
TamarDolaberidze <dolaberidzetamari@gmail.com>
2024-09-11 21:28:58 +0400

    homework1

---
+++ homework1/task_3.py
@@ -0,0 +1,10 @@
+x = float(input("enter first parameter: "))
+y = float(input("enter second parameter: "))
+z = float(input("enter third parameter: "))
+print ((x+y+z)/2, x+y+z)
+
+
+
+
+
+
---
+++ homework1/task_4.py
@@ -0,0 +1,5 @@
+c = float(input("enter a celsius: "))
+f = (c*9/5)+32
+print (f)
+
+
