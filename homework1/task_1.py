3f58fbb05534ba06218f0750c155d074524b2bfa
TamarDolaberidze <dolaberidzetamari@gmail.com>
2024-09-11 21:35:42 +0400

    homework-1-branch

--- homework1/task_1.py
+++ homework1/task_1.py
@@ -1,6 +1,6 @@
 first_name = input("Enter a name: ")
 last_name = input("Enter a last name: ")
-age = input("Enter age: ") 
+age = input("Enter age: ")
 print ("გამარჯობა,", first_name, last_name, "თქვენ ხართ", age, "წლის.")
 
 
--- homework1/task_3.py
+++ homework1/task_3.py
@@ -1,7 +1,7 @@
 x = float(input("enter first parameter: "))
 y = float(input("enter second parameter: "))
 z = float(input("enter third parameter: "))
-print ((x+y+z)/2, x+y+z) 
+print ((x+y+z)/2, x+y+z)
 
 
 
