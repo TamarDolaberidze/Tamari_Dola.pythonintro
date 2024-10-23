67487cfc201bea52dd6bc1f6809f1a517f4505ac
tdolaberidze <dolaberidzetamari@gmail.com>
2024-10-23 23:39:30 +0400

    Update task_3.py

--- homework7/task_3.py
+++ homework7/task_3.py
@@ -1,14 +1,11 @@
 word = input("enter a text: ")
-
-if len(word) %2 == 0:
-    first_char = word[int(len(word)/2)-1]  #აქ int რატომ დამჭირდა ვერ ვიგებ. float როგორ იქნება len(word)/2, თუ ლუწია len(word)?
-    second_char = word[int(len(word)/2)]
-    print (first_char, second_char)
-else:
-    i = 0
-    while i < 5:
-       first_char = word[0:1]
-       second_char = word[len(word)//2+1]
-       print(first_char, second_char)
-       i+=1
-    
+i = 0
+while i < 5:
+    if len(word) %2 == 0:
+        first_char = word[int(len(word)/2)-1] 
+        second_char = word[int(len(word)/2)]
+        print (fisrst_char, second_char)
+    else:
+        print(word[0], word[int(len(word)/2)], word[-1])
+    i += 1
+        
